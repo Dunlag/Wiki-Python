78808b0338fe7ca74c806346e4fec6b6c940f238
Fernando <fernando.pinilla85@gmail.com>
2025-08-19 13:03:54 +0200

    Remove unused HTML and Python files; add main CSS and update templates for Flask app

--- 12 - DemoWeb/appweb/web1.py
+++ 12 - DemoWeb/appweb/web1.py
@@ -1,4 +1,4 @@
-from flask import Flask  # Importa la clase Flask del paquete flask
+from flask import Flask, render_template  # Importa la clase Flask del paquete flask
 
 app = Flask(__name__)  # Crea una instancia de la aplicación Flask
 
@@ -7,13 +7,13 @@ app = Flask(__name__)  # Crea una instancia de la aplicación Flask
 @app.route("/")
 def home():
     # Esta función se ejecuta cuando se accede a la página principal
-    return "Home page!"  # Responde con este texto
+    return render_template("home.html")
 
 # Define la ruta principal de la web ('/')
 @app.route("/about/")
 def about():
     # Esta función se ejecuta cuando se accede a la página principal
-    return "About content goes here!"  # Responde con este texto
+    return render_template("about.html")
 
 
 # Ejecuta la aplicación solo si este archivo es el principal
